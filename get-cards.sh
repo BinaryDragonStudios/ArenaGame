7e6cf09be53af24463fe70cf648fd5a6b155aa32
jared0x90@gmail.com <root@hs.jwd.me>
2016-03-27 15:06:53 -0400

    743 cards working

--- get-cards.sh
+++ get-cards.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+rm cards.json
+rm cards.collectible.json
 wget https://api.hearthstonejson.com/v1/latest/enUS/cards.json
-
+wget https://api.hearthstonejson.com/v1/latest/enUS/cards.collectible.json
