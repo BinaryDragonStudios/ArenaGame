9834f496c0d0da640ec7bef269a07d72375dc581
root <root@hs.jwd.me>
2016-04-03 09:27:54 -0400

    Improvements to handle misspelled cards ++ [Allikin]

--- parse-csv.py
+++ parse-csv.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python
 
-import sys
 import sqlite3
 
 def main():
     
     # Connect to our database and create scores table if it doesn't exist
     db = sqlite3.connect('game.sqlite')
+    db.text_factory = str
     c = db.cursor()
     query = "CREATE TABLE IF NOT EXISTS scores('card_game_id', 'draft_class', 'score', 'modifier', 'score_version','pick_counter','offer_counter', UNIQUE(card_game_id,draft_class))"
     c.execute(query)
     
     score_version = '20150703' # Maybe make this a parameter?
-
-    select_card_id_query = 'SELECT card_game_id FROM cards WHERE card_name_en = ? LIMIT 1'
-    
     class_list = ['druid', 'hunter', 'mage', 'paladin', 'priest', 'rogue', 'shaman', 'warlock', 'warrior']
     
     for draft_class in class_list:
@@ -26,11 +23,9 @@ def main():
             # Boolean keeping track of the value (card name or score); first value is always card name
             is_card_name = True
             
-            # Keep track of the cards processed (by card_game_id)
+            # Keep track of the cards processed (by card_game_id) and cards not found (by name)
             processed = []
-            
-            # Card counter
-            card_count = 0
+            cards_not_found = []
             
             for line in csv_file:
                 # All rows with relevant data starts with ; or  -
@@ -47,12 +42,19 @@ def main():
                         
                             # If the value is the card name: get the card ID from the cards table
                             if is_card_name:
-                                c.execute(select_card_id_query, (value,))
+                                c.execute('SELECT card_game_id FROM cards WHERE card_name_en = ? LIMIT 1', (value,))
                                 row = c.fetchone()
-                                card_game_id = row[0]                        
+                                
+                                # If the card name is mistyped in the score data, we need to handle it
+                                if row is None:
+                                    cards_not_found.append(value)
+                                    card_game_id = 'not_found'
+                                else:  
+                                    card_game_id = row[0]                        
+                                
                                 processed.append(card_game_id)
-
-                                # If the value is not the card name, it's the associated score
+                                  
+                            # If the value is not the card name, it's the associated score
                             else:
                             
                                 # The score may have a modifier, indicated by a *, we store them seperatly
@@ -62,25 +64,42 @@ def main():
                                 # Now that we have all available card data, insert them into the database
                                 card_data = (card_game_id, draft_class, score, modifier, score_version, None, None)
                                 c.execute('REPLACE INTO scores VALUES(?,?,?,?,?,?,?)', card_data)
-                                card_count+=1
                                 
                             # Toggle is_card_name
                             is_card_name = not is_card_name            
                         
         # Discover duplicates
         dupes = set([x for x in processed if processed.count(x) > 1])
-        print 'Number of cards processed for ' + draft_class + ': ' + str(card_count - len(dupes))
+        
+        # Print output
+        print ''
+        print draft_class.upper()
+        print 'Cards processed: ' + str(len(processed) - len(dupes) - len(cards_not_found))
 
+        # If we have duplicate entries, get the card name(s) from the cards table and print them
         if len(dupes) > 0:
-            print 'Duplicates: ' + str(dupes)
-       
+            values = ','.join(dupes)
+            c.execute("SELECT card_name_en from cards WHERE card_game_id IN (?)", (values,))
+            print 'Duplicates     : ' + ', '.join([r[0] for r in c.fetchall()])
+        
+        # If we have misspelled cards in out data print them
+        if len(cards_not_found) > 0:
+            print 'Not found      : ' + ', '.join(cards_not_found)
+            
         csv_file.close()                
     
     print ''
-    c.execute('SELECT count(*) from scores')
+    
+    # Delete cards that are misspelled and thus not connected to card table
+    c.execute("DELETE from scores WHERE card_game_id = 'not_found'")
+    c.execute("VACUUM")
+    
+    # Print number of scores recorded in table
+    c.execute("SELECT count(*) from scores")
     row = c.fetchone()
     print 'Number of cards in scores table: ' + str(row[0])
     
+    # Commit database executes
     try:
         db.commit()
     except sqlite3.Error as e:
