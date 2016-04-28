9485dd01ac85d8d1bf84c052d3a4e865cd784c8f
root <root@hs.jwd.me>
2016-04-28 10:36:30 -0400

    old gods prep

--- get-cards.sh
+++ get-cards.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 rm cards.json
 rm cards.collectible.json
+rm cardtier.json
 wget https://api.hearthstonejson.com/v1/latest/enUS/cards.json
 wget https://api.hearthstonejson.com/v1/latest/enUS/cards.collectible.json
+wget https://raw.githubusercontent.com/rembound/Arena-Helper/master/data/cardtier.json
