c52f175b1940a79f1d19f01580b208c0ea9b031a
root <root@hs.jwd.me>
2016-05-01 10:04:30 -0400

    removed node js code

--- node-old/sqlite-stuff.js
+++
@@ -1,9 +0,0 @@
-var sqlite3 = require('sqlite3').verbose(); 
-var db = new sqlite3.Database('game.sqlite');
-
-db.all("SELECT card_game_id FROM cards", function(err, rows) {
-        rows.forEach(function (row) {
-            console.log(row.card_game_id);
-        })
-    });
-db.close();
